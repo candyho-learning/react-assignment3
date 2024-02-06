fcf43a3ea2a35a157568785d2abad5a2740171ad
Candy Ho <candyho.learning@gmail.com>
2024-02-06 15:18:11 +0800

    fixed logo and icon not showing issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "./waveless-logo.svg";
 
 export default function NavBar({ openMenu }) {
   return (
     <nav>
-      <img id="logo" src="/waveless-logo.svg" alt="logo" />
+      <img id="logo" src={logo} alt="logo" />
       <i className="fa-solid fa-bars hamburger-menu" onClick={openMenu}></i>
       <div className="menu-items">
         <a href="#">Item1</a>
